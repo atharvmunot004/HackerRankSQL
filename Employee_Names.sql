a005ad820c0e4813dc854f119a45b91b40257ecb
atharvmunot004 <munot.atharv@gmail.com>
2024-08-09 20:52:24 +0530

    Create Employee_Names.sql

---
+++ Employee_Names.sql
@@ -0,0 +1,10 @@
+/*
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+*/
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+select name
+from employee
+order by name;
