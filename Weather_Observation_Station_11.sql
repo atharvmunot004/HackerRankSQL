b64aeffc88938b9e1182c3d1b563ff50d39d3c08
atharvmunot004 <munot.atharv@gmail.com>
2024-08-09 20:37:08 +0530

    Create Weather_Observation_Station_11.sql

---
+++ Weather_Observation_Station_11.sql
@@ -0,0 +1,7 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+select distinct city
+from station
+where (city not like '%a' and city not like '%e' and city not like '%i' and city not like '%o' and city not like '%u') or (city not like 'A%' and city not like 'E%' and city not like 'I%' and city not like 'O%' and city not like 'U%');
